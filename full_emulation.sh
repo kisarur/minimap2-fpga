7df02d188a6a87e8ea8e480ab0fd394402f60be0
Kisaru Liyanage <kisarul@scylla.cse.unsw.EDU.AU>
2022-06-22 14:33:23 +1000

    add emulation support

---
+++ full_emulation.sh
@@ -0,0 +1,12 @@
+aocl help > /dev/null || source /opt/intelFPGA_pro/21.2/hld/init_opencl.sh
+export ACL_BOARD_VENDOR_PATH=/home/kisarul/a10-bsp/
+
+# compile kernel code for emulation
+rm -rf bin/minimap2_opencl_emul.aocx
+aoc -march=emulator -v device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocx
+
+# compile host code
+make
+
+# run host with emulation
+bin/host -ax map-ont -t 4 /storage/hasindu/genome/human_genome/hg38noAlt.idx /storage/hasindu/NA12878_prom_subsample/reads.fastq > /dev/null

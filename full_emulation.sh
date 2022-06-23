4a943a067899cf611a871c6350208bedab114264
Kisaru Liyanage <kisarul@scylla.cse.unsw.EDU.AU>
2022-06-24 00:29:58 +1000

    add optional intermediate report generation command to emulation flow

--- full_emulation.sh
+++ full_emulation.sh
@@ -3,10 +3,11 @@ export ACL_BOARD_VENDOR_PATH=/home/kisarul/a10-bsp/
 
 # compile kernel code for emulation
 rm -rf bin/minimap2_opencl_emul.aocx
+#aoc -rtl device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocr -board=a10gx -report # for intermediate compilation while generating reports
 aoc -march=emulator -v device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocx
 
 # compile host code
 make -j
 
 # run host with emulation
-bin/host -ax map-ont -t 4 /storage/hasindu/genome/human_genome/hg38noAlt.idx /storage/hasindu/NA12878_prom_subsample/reads.fastq > /dev/null
+bin/host -ax map-ont -t 40 /storage/hasindu/genome/human_genome/hg38noAlt.idx /storage/hasindu/NA12878_prom_subsample/reads.fastq > /dev/null

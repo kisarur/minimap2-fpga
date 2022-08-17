2f0510a6a33cae3faa6de16ac5c33c9b6247a6e5
Kisaru Liyanage <kisarur@gmail.com>
2022-08-17 13:56:03 +1000

    add emulation support on brenner-fpga

--- full_emulation.sh
+++ full_emulation.sh
@@ -1,14 +1,25 @@
-aocl help > /dev/null || source /opt/intelFPGA_pro/21.2/hld/init_opencl.sh
-export ACL_BOARD_VENDOR_PATH=/home/kisarul/a10-bsp/
+# environment setup for scylla
+# aocl help > /dev/null || source /opt/intelFPGA_pro/21.2/hld/init_opencl.sh
+# export ACL_BOARD_VENDOR_PATH=/home/kisarul/a10-bsp/
+
+# environment setup for brenner-fpga
+aocl help > /dev/null || source init_env.sh
+export ACL_BOARD_VENDOR_PATH=/home/kisliy/ocl_emulator/a10_ref_install/
+export LD_LIBRARY_PATH=/home/kisliy/opt/gcc-9.1.0/lib64:$LD_LIBRARY_PATH # for newer libstdc++.so required by OpenCL Emulator 
 
 # compile kernel code for emulation
-rm -rf bin/minimap2_opencl_emul.aocx
-#aoc -rtl device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocr -board=a10gx -report # for intermediate compilation while generating reports
+rm bin/minimap2_opencl_emul.aocx
 aoc -march=emulator -v device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocx
 
-# compile host code
+# for intermediate compilation while generating OpenCL reports 
+# aoc -rtl device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocr -board=a10gx -report # scylla
+# aoc -rtl device/minimap2_opencl.cl -o bin/minimap2_opencl_emul.aocr -board=pac_a10 -report # brenner-fpga
+
+# compile host code for hardware emulation
 sed -i '/static bool use_emulator/c\static bool use_emulator = true;' chain_hardware.cpp
+make clean
 make -j
 
-# run host with emulation
-bin/host -ax map-ont -t 40 /storage/hasindu/genome/human_genome/hg38noAlt.idx /storage/hasindu/NA12878_prom_subsample/reads.fastq > /dev/null
+# run host with hardware emulation
+# bin/host -ax map-ont -t 20 /storage/hasindu/genome/human_genome/hg38noAlt.idx /storage/hasindu/NA12878_prom_subsample/reads.fastq > /dev/null # scylla
+bin/host -ax map-ont -t 20 /share/ScratchGeneral/kisliy/minimap2_inout/hg38noAlt.idx /share/ScratchGeneral/kisliy/minimap2_inout/reads.fastq > /dev/null # brenner-fpga

3b82616d66961efad1b0547db5267dc5c8e2bc6e
Kisaru Liyanage <kisarur@gmail.com>
2023-02-03 05:42:34 +0000

    fix bugs in hw compile script

--- 1_compile_fpga.sh
+++ 1_compile_fpga.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # "$AWS_FPGA_REPO_DIR/vitis_setup.sh" should be sourced first (i.e. source $AWS_FPGA_REPO_DIR/vitis_setup.sh)
 
-FILE=minimap2_opencl
-
-# make cleanall // to remove all the previously built hardware files 
+make cleanall # to remove all the previously built hardware files 
 make build TARGET=hw DEVICE=$AWS_PLATFORM 
-cp build_dir.hw.*.xclbin bin/
+cp build_dir.hw.*/minimap2_opencl.xclbin bin/
